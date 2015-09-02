ebfe89ee0e8720239370979d4ed22e8fca557364
persistforever <caocao7066@outlook.com>
2015-09-02 15:31:36 +0800

    kdnet

--- kdnet.net/code/page.py
+++ kdnet.net/code/page.py
@@ -5,12 +5,13 @@ according root topic to catch each question and store as a
 '''
 
 import urllib2
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup, Tag
 import time
 import os
 import random
 import sys
 import ConfigParser
+import codecs
 
 
 class Conf :
@@ -38,83 +39,114 @@ class Conf :
 
 class KDNet :
 	# attributes
-	mainurl = ''
-	qturl = 'http://www.zhihu.com/question/'
-	topictree = dict()
+	homepage = 'http://club.kdnet.net/index.asp'
+	mainurl = 'http://club.kdnet.net'
+	maindir = 'data'
 
-	def __init__(self, mainurl) :
-		self.mainurl = mainurl
-		self.topictree['19776749'] = []
+	def urlOpen(self, url) :
+		content = ''
+		with codecs.open(url, 'r', 'gb18030') as fo :
+			for line in fo.readlines() :
+				content += line.strip().encode('utf-8') + '\n'
+		return content
 
-	def getQuestion(self, url) :
-		content = urllib2.urlopen(url).read()
+	def getQAPair(self, url) :
+		qalist = []
+		# content = urllib2.urlopen(url).read()
+		content = self.urlOpen(url)
 		soup = BeautifulSoup(content)
-		qtdata = soup.contents[2].contents[3].contents[47].contents[3].findAll('div')
-		for div in qtdata :
-			if 'class' in [t[0] for t in div.attrs] :
-				if div['class'] == 'reply-box' :
-					if div.contents[3].contents[1]['class'] == 'replycont-box-r' :
-						print div.contents[3].contents[1].contents[1].contents[0].encode('gb18030')
-		return qtdata
+		alltags = soup.findAll(True)
+		for tag in alltags :
+			if tag.name == 'div' :
+				if 'class' in [t[0] for t in tag.attrs] :
+					if tag['class'] == 'reply-box' or tag['class'] == 'reply-box nobg' :
+						if isinstance((tag.contents[3].contents[1].contents[1].contents[0]), Tag) == True :
+							qalist.append([tag.contents[3].contents[1].contents[1].contents[0].contents[0].contents[1], 
+								tag.contents[3].contents[1].contents[1].contents[1]])
+		return qalist
 	
 	def getQuestionList(self, url) :
-		topic, question, count = [], [], []
-		content = urllib2.urlopen(url).read()
+		qslist = []
+		# content = urllib2.urlopen(url).read()
+		content = self.urlOpen(url)
 		soup = BeautifulSoup(content)
-		alist = soup.contents[2].contents[3].contents[5].contents[1].contents[1].contents[5].contents[1].findAll('a')
-		metalist = soup.contents[2].contents[3].contents[5].contents[1].contents[1].contents[5].contents[1].findAll('meta')
-		t = '19776749'
-		qnum = 0
-		for i in range(len(alist)) :
-			if alist[i]['href'].split('/')[-2] == 'question' :
-				self.topictree[t].append([alist[i]['href'].split('/')[-1], int(metalist[qnum*2]['content'])])
-				t = '19776749'
-				qnum += 1
-			else :
-				t = alist[i]['href'].split('/')[-1]
-				if t not in self.topictree :
-					self.topictree[t] = []
+		alltags = soup.findAll(True)
+		for tag in alltags :
+			if tag.name == 'td' and tag['class'] == 'subject' :
+				href, page = '', 1
+				allspan = tag.findAll('span')
+				for span in allspan :
+					if span['class'] == 'f14px' :
+						alla = span.findAll('a')
+						for a in alla :
+							href = a['href']
+					elif span['class'] == 'c-alarm' :
+						alla = span.findAll('a')
+						for a in alla :
+							page = int(a.contents[0])
+				qslist.append([href, page])
+		return qslist
 
-	def getPageList(self, startpage, maxpage) :
-		for page in range(startpage, startpage + maxpage) :
-			try:
-				self.getQuestionList(self.mainurl + '?page=' +str(page))
-				print 'read page', page, 'finished ...'
-			except Exception, e:
-				pass
-			if (page + 1) % 100 == 0 :
-				time.sleep(int(random.random()*100%30))
+	def getTopicList(self, url) :
+		topiclist = []
+		content = urllib2.urlopen(url).read()
+		# content = self.urlOpen(url)
+		soup = BeautifulSoup(content)
+		alltags = soup.findAll(True)
+		for tag in alltags :
+			if tag.name == 'h2':
+				href, page = '', ''
+				allspan = tag.findAll('span')
+				for span in allspan :
+					print span
+					alla = span.findAll('a')
+					for a in alla :
+						href = self.mainurl + a['href']
+				if len(tag.contents) >= 3 :
+					print tag.contents[3]
+					page = int(tag.contents[3].split(u'\xd6\xf7\xcc\xe2\xa3\xba')[1].split(u')')[0].strip())/50+1
+					if href not in topiclist :
+						topiclist.append([href, page])
+		return topiclist
 
-	def storeTopic(self, maindir) :
-		num = 0
-		for topic in self.topictree :
-			subdir = maindir + '/' + topic
-			if not os.path.exists(subdir) :
-				os.mkdir(subdir)
-			for qt, count in self.topictree[topic] :
-				if count != 0 :
-					try:
-						filepath = subdir + '/' + qt + '.txt'
-						qtdata = self.getQuestion(self.qturl + qt)
-						num += 1
-						with open(filepath, 'w') as fw :
-							fw.write(str(qtdata))
-					except Exception, e:
-						pass
-				if (num + 1) % 1000 == 0 :
-					time.sleep(int(random.random()*100%30))
-			print 'write topic ', topic, 'finished ...' 
+	def storeQAList(self, storepath, qalist) :
+		with codecs.open(storepath, 'w') as fw :
+			for q, a in qalist :
+				fw.writelines('!!\n')
+				fw.writelines('KNOWLEDGE\n')
+				fw.writelines('QT' + str(q) + '\n')
+				fw.writelines('AS' + str(a) + '\n')
+				fw.writelines('SC1.0\n')
+				fw.writelines('KNOWLEDGE\n')
 
-	def process(self, startpage, deltapage, subdelta) :
-		page = startpage
-		while page < (startpage + deltapage) :
-			zhihu.getPageList(startpage, subdelta)
-			print zhihu.topictree
-			zhihu.storeTopic('E://data/zhihu')
-			page += subdelta
+	def storeTopicList(self, storepath, topiclist) :
+		with codecs.open(storepath, 'w') as fw :
+			for topicurl, topicpage in topiclist :
+				fw.writelines(str(topicurl) + '\t' + str(topicpage) + '\n')
+
+	def process(self) :
+		topiclist = self.getTopicList(self.homepage)
+		topiclistpath = self.maindir + '/topiclist'
+		self.storeTopicList(topiclistpath, topiclist)
+		'''
+		for topicurl, topicpage in topiclist :
+			topicid = topicurl.split('=')[-1]
+			storeqalist = []
+			for page in range(1, topicpage) : # every topic
+				qslisturl = topicurl + '&page=' + str(page)
+				qslist = self.getQuestionList(qslisturl)
+				for qsurl, qspage in qslist : # every question
+					for p in range(1, qspage) :
+						pairurl = qsurl + '&page=' + str(p)
+						qalist = self.getQAPair(pairurl)
+						storeqalist.extend(qalist)
+				if page % 100 == 0 :
+					topicdir = self.maindir + '/' + topicid
+					storepath = topicdir + '/' + page
+					self.storeQAList(storepath, storeqalist)'''
 
 
 
-
-kdnet = KDNet('http://club.kdnet.net/dispbbs.asp')
-kdnet.getQuestion('http://club.kdnet.net/dispbbs.asp?id=11139095&boardid=1')
+kdnet = KDNet()
+# kdnet = KDNet('../input/topic.txt')
+kdnet.process()
